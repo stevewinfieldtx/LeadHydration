6c37c8bbc31e7e669fd4c51a5a48d590f22dab4e
Steve Winfield <stevewinfieldtx@gmail.com>
2026-04-05 22:21:47 +0700

    adding demo

---
+++ git_push.bat
@@ -0,0 +1,6 @@
+cd /d C:\Users\steve\Documents\LeadHydration
+git add server.js public/index.html
+git commit -m "Interactive cards overhaul: score sorting, drip campaigns, pain explanations, PAM popup, demo thread generator, AI strategic insights, demo template button"
+git push
+echo DONE
+pause
